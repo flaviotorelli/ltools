2d13d844af6cd70c946b415156abcc990c87ddd6
flavio <flavio.torelli@gmail.com>
2013-12-30 01:25:09 -0200

    comparing meta tags

--- DOM/comparemetatags.js
+++ DOM/comparemetatags.js
@@ -3,29 +3,29 @@ var result = data.split('##');
   var page_title = result[0];
   var meta_description = result[1];
   var meta_keywords = result[2];
+  var error = false;
 
-  // comparing titles  
   if (page_title != jQuery('title').html()) {
     alert('Page title does NOT match!');
-  }
-  else {
-    alert('Page title MATCHES!');
+    error = true;
   }
 
-  // comparing description
-  if (meta_description != 'null' && meta_description != jQuery("meta[name=description]").attr('content')) {
-    alert('Meta description does NOT match!');
-  }
-  else {
-    alert('Meta description MATCHES!');
+  if (meta_description != 'null' || jQuery("meta[name=description]").length) {
+    if (meta_description != jQuery("meta[name=description]").attr('content')) {
+      alert('Meta description does NOT match!');
+      error = true;
+    }
   }
 
-  // comparing keywords
-  if (meta_keywords != 'null' && meta_keywords != jQuery("meta[name=keywords]").attr('content')) {
-    alert('Meta keywords does NOT match!');
+  if (meta_keywords != 'null' || jQuery("meta[name=keywords]").length > 0) {
+    if (meta_keywords != jQuery("meta[name=keywords]").attr('content')) {
+      alert('Meta keywords does NOT match!');
+      error = true;
+    }
   }
-  else {
-    alert('Meta keywords MATCHES!');
+
+  if (!error) {
+    alert('Meta tags are equal!');
   }
-  
+
 });
